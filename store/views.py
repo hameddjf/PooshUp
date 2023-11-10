7ac05e76b147a268f0fd0f59c8f4a9b5065c9ee8
hameddjf <99538932+hameddjf@users.noreply.github.com>
2023-11-10 20:07:00 +0330

    Added category

--- store/views.py
+++ store/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Product
+from category.models import Category
 # Create your views here.
 
-def store(request , category_slug=None):
+def store(request , category_slug=None ):
     categories          = None
     products            = None
 
     if category_slug   != None:
-        categories      = get_object_or_404(Category , category_slug = slug)
-        products        = Product.objexts.filter(category=categories,is_available=True)
+        categories      = get_object_or_404(Category , slug = category_slug) 
+        products        = Product.objects.filter(category=categories,is_available=True)
         product_count   = products.count()
-
-    products            = Product.objects.all().filter(is_available=True)
-    product_count       = products.count()
+    else:
+        products        = Product.objects.all().filter(is_available=True)
+        product_count   = products.count()
     context             = {
         'products': products,
         'product_count' : product_count,
