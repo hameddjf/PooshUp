7fdc9bce3502529114c866df4304f91132fdd8e0
hameddjf <99538932+hameddjf@users.noreply.github.com>
2024-02-23 08:53:19 +0330

    products-admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Product, Category
+
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'slug', 'price', 'stock',
+                    'category', 'created_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', )
+
+
+admin.site.register(Category, CategoryAdmin)
